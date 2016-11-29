5d7deadb2c909fe0774ceb8f4fe6277d0c903dca
Alina <alina.trepacheva@area9.dk>
2016-11-30 02:15:09 +0300

    k3

---
+++ WithdrawData.java
@@ -0,0 +1,40 @@
+package sc.admin;
+
+/**
+ * Created by alina on 15.11.2015.
+ */
+public class WithdrawData {
+
+    private long gameCnt;
+    private long totalIn;
+    private long totalOut;
+
+
+    // Constructor
+     public WithdrawData(){
+
+     }
+
+    public void setWithdrawData(long gameCnt, long totalIn,long totalOut) {
+        this.gameCnt = gameCnt;
+        this.totalIn = totalIn;
+        this.totalOut = totalOut;
+    }
+
+
+    // Getters
+
+    public long getTotalIn() {
+        return totalIn;
+    }
+
+    public long getGameCnt() {
+        return gameCnt;
+    }
+
+    public long getTotalOut() {
+        return totalOut;
+    }
+
+
+}
